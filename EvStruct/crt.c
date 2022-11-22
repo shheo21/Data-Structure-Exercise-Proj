dc55c6719fd2ad30ba919938e795258f5fcafb2e
shheo21 <randy0007@kw.ac.kr>
2022-11-23 02:33:23 +0900

    [Mod] 일정 파일 생성 작업 중

--- EvStruct/crt.c
+++ EvStruct/crt.c
@@ -7,10 +7,13 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <ncurses.h>
+#include <time.h>
 
-#define PERMS 755
+#define DPERMS 755
+#define FPERMS 644
 
-char* schedloc = "~/.local/dsep/sched";
+//char* schedloc = "~/.local/dsep/sched";
+char* schedloc = "./sched";
 //char temp[256] = "";
 
 typedef struct sched {
@@ -29,27 +32,46 @@ void makeCont(int year) {
 	isch.year = year;
 	isch.month = 1;
 	isch.day = 1;
-	char buf[12];
-	
+	char buf[12] = { '\0', };
 	chdir(schedloc);
-	char cwd[256];
+	char cwd[256] = { '\0', };
 	strcat(cwd, schedloc);
-	mkdir(strcat(cwd, "/year"), PERMS);
+	if (mkdir(strcat(cwd, "/year"), DPERMS) < 0)
+		return;
 	chdir(cwd);
 	for (; isch.month <= 12; ++isch.month) {
 		sprintf(buf ,"%d", isch.month);
-		mkdir(buf, PERMS);
+		mkdir(buf, DPERMS);
 		memset(buf, '\0', sizeof(buf));
 	}
 
+}
+
+void writesch(int yr, int mh, int dd, int sth, int stm, int endh, int endm, char* note) {
+	int fd;
+	char* fname = "";
+	sprintf(fname, "%ld;%d;%d;%d;%d:%d~%d:%d", time(NULL), yr, mh, dd, sth, stm, endh, endm);
+	if ((fd = open(fname, O_RDWR|O_CREAT, FPERMS)) < 0) {
+		perror("Could not write schedule!");
+		return;
+	}
+	write(fd, note, strlen(note));
+	close(fd);
+}
+
+ssched readsch(int yr, int mh, int dd, int sth, int stm, int endh, int endm) {
+	ssched res;
+	char* schname = "";
+
+	
 }
 
 int main(int argc, char *argv[])
 {
 	DIR* dirp;
 	struct dirent* dentry;
-
-
+	makeCont(2022);
+	
 
 	return 0;
 }
