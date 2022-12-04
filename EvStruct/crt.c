c5cb6dd0bcfc408859dc2ec1095db482e51faa8b
shheo21 <randy0007@kw.ac.kr>
2022-12-04 14:05:28 +0900

    [Mod] 일정 출력 입력 값 날짜로 수정

--- EvStruct/crt.c
+++ EvStruct/crt.c
@@ -27,6 +27,16 @@ typedef struct sched {
 	char note[21];
 } ssched;
 
+char* substr(char* str, int st, int ed)
+{
+	char* ss = (char *)malloc(sizeof(char) * (ed - st + 2));
+	strncpy(ss, str + st, ed - st + 1);
+	ss[ed - st + 1] = '\0';
+	
+	return ss;
+}
+
+// 일정 저장 디렉토리 구조 생성
 void makeCont(int year) {
 	ssched isch;
 	isch.year = year;
@@ -35,6 +45,7 @@ void makeCont(int year) {
 	char buf[12] = { '\0', };
 	chdir(schedloc);
 	char cwd[256] = { '\0', };
+	int days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	strcat(cwd, schedloc);
 	if (mkdir(strcat(cwd, "/year"), DPERMS) < 0)
 		return;
@@ -42,6 +53,13 @@ void makeCont(int year) {
 	for (; isch.month <= 12; ++isch.month) {
 		sprintf(buf ,"%d", isch.month);
 		mkdir(buf, DPERMS);
+		snprintf(buf, sizeof(buf), "%d", isch.month);
+		chdir(buf);
+		for (int day = 1; day <= days[isch.month - 1]; ++day)
+		{
+			snprintf(buf, sizeof(buf), "%d", day);
+			mkdir(buf, DPERMS);
+		}
 		memset(buf, '\0', sizeof(buf));
 	}
 
@@ -50,7 +68,7 @@ void makeCont(int year) {
 void writesch(ssched sch) {
 	int fd;
 	char* fname = "";
-	sprintf(fname, "%d;%d;%d;%d:%d~%d:%d", sch.year, sch.month, sch.day, sch.sthour, sch.stmin, sch.endhour, sch.endmin);
+	sprintf(fname, "%d/%d/%d/%d:%d~%d:%d", sch.year, sch.month, sch.day, sch.sthour, sch.stmin, sch.endhour, sch.endmin);
 	if ((fd = open(fname, O_RDWR|O_CREAT, FPERMS)) < 0) {
 		perror("Error while writing schedule!");
 		return;
@@ -59,12 +77,14 @@ void writesch(ssched sch) {
 	close(fd);
 }
 
+// 날짜는 01;11; 과 같이 앞에 0을 붙여서 표기
 ssched readsch(int yr, int mh, int dd, int sth, int stm, int endh, int endm) {
 	int fd;
 	size_t rsize;
 	ssched res;
+	memset(&res, 0, sizeof(res));
 	char* schname = "";
-	sprintf(schname, "%d;%d;%d;%d:%d~%d:%d", yr, mh, dd, sth, sth, endh, endm);
+	sprintf(schname, "%d/%d/%d/%d:%d~%d:%d", yr, mh, dd, sth, sth, endh, endm);
 	if ((fd = open(schname, O_RDWR)) < 0) {
 		perror("Error in reading schedule!");
 		memset(&res, 0, sizeof(ssched));
@@ -102,7 +122,7 @@ int printSched(char* date)
 		read(fd, buf, sizeof(buf));
 		close(fd);
 
-		printf("%s\n", buf);
+		printf("%s: %s\n", fname, buf);
 	}
 
 	return 0;
